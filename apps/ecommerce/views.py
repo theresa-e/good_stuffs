b1d07b51b463911b58c36823a9d1d3811a80e96d
Theresa Eliger <910theresa@gmail.com>
2018-10-02 10:58:39 -0700

    Added templates, static folders, index.html page

--- apps/ecommerce/views.py
+++ apps/ecommerce/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    response = "AYOO HOLLA MAMA"
-    return HttpResponse(response)
+    print('User navigated to main page.')
+    return render(request, 'ecommerce/index.html')
